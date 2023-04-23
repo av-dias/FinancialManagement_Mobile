211185722ff66d21d1256948acaf8c68b3e15202
av.dias <av.dias@campus.fct.unl.pt>
2023-04-23 18:12:25 +0100

    app.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./pages/login";
+import Home from "./pages/home";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d4e4fc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
