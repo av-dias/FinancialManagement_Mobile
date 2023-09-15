4750da1baf558aea7534e757c082544cb2161f1a
av.dias <av.dias@campus.fct.unl.pt>
2023-09-15 18:38:31 +0100

    [App] Responsive refractor header icon server and exit

--- components/header.js
+++ components/header.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 import { _styles } from "../utility/style";
 import { saveToStorage, getFromStorage } from "../utility/secureStorage";
 import { getServerStatus } from "../functions/basic";
+import { horizontalScale, verticalScale, moderateScale } from "../utility/responsive";
 
 export default function Header(props) {
   const styles = _styles;
@@ -21,7 +22,7 @@ export default function Header(props) {
 
   const serverOn = () => {
     return (
-      <View style={styles.icon}>
+      <View style={styles.iconLeft}>
         <MaterialCommunityIcons name="server" size={18} color="gray" />
       </View>
     );
@@ -29,7 +30,7 @@ export default function Header(props) {
 
   const serverOff = () => {
     return (
-      <View style={styles.icon}>
+      <View style={styles.iconLeft}>
         <MaterialCommunityIcons name="server-off" size={18} color="gray" />
       </View>
     );
@@ -37,7 +38,7 @@ export default function Header(props) {
 
   return (
     <View style={styles.header}>
-      <View style={{ flex: 1, flexDirection: "row", gap: 10 }}>
+      <View style={{ flex: 1, flexDirection: "row", gap: horizontalScale(10) }}>
         <View style={styles.icon}>
           <Ionicons
             name="ios-menu"
@@ -52,16 +53,18 @@ export default function Header(props) {
           <Text style={styles.headerText}>{props.email}</Text>
         </View>
       </View>
-      {server == "on" ? serverOn() : serverOff()}
-      <View style={styles.icon}>
-        <MaterialIcons
-          name="logout"
-          size={20}
-          color="white"
-          onPress={() => {
-            props.navigation.navigate("Login");
-          }}
-        />
+      <View style={{ flexDirection: "row", gap: horizontalScale(10) }}>
+        {server == "on" ? serverOn() : serverOff()}
+        <View style={styles.iconLeft}>
+          <MaterialIcons
+            name="logout"
+            size={20}
+            color="white"
+            onPress={() => {
+              props.navigation.navigate("Login");
+            }}
+          />
+        </View>
       </View>
     </View>
   );
