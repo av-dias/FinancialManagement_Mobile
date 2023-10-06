a83f76e934abfc1987bceedffaf9818d3ca46917
av.dias <av.dias@campus.fct.unl.pt>
2023-10-06 18:32:28 +0100

    [Calendar] Polish design

--- utility/colors.js
+++ utility/colors.js
@@ -1,9 +1,9 @@
 export const color = {
   backgroundDark: "rgba(26, 34, 56,1)",
   backgroundLight: "rgba(227, 242, 253,1)",
+  complementary: "rgba(0, 131, 233, 0.04)",
   glass: "rgba(184, 191, 214, 0.1)",
   glassBorder: "rgba(211,211,211,1)",
-  complementary: "rgba(0, 131, 233, 0.04)",
   calendar: "transparent",
   calendarBorder: "rgba(20, 108, 148, 0.3)",
 };
