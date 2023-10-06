b0e9dcb0ae162598edf6859ce23ab13f76126943
av.dias <av.dias@campus.fct.unl.pt>
2023-10-06 09:55:22 +0100

    [List] Reduced archived item list text size and style consistency adjustement

--- utility/colors.js
+++ utility/colors.js
@@ -3,7 +3,7 @@ export const color = {
   principal: "#2296F3",
   backgroundDark: "#1a2238",
   backgroundLight: "#e3f2fd",
-  backgroundSecundary: "rgba(184, 191, 214, 0.3)",
+  backgroundSecundary: "rgba(184, 191, 214, 0.08)",
   backgroundComplementary: "rgba(0, 131, 233, 0.04)",
   button: "#1976D2",
   chart: "#284387",
