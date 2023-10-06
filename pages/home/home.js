dc5d491d3aa82d909334953dfe365e2a63c060f0
av.dias <av.dias@campus.fct.unl.pt>
2023-10-06 09:25:34 +0100

    [Home] Table alignment

--- pages/home/home.js
+++ pages/home/home.js
@@ -35,7 +35,7 @@ export default function Home({ navigation }) {
 
   const state = {
     tableHead: ["", "Type", "Value"],
-    tableFlex: [1, 3, 2],
+    tableFlex: [1, 3, 1],
   };
 
   useFocusEffect(
