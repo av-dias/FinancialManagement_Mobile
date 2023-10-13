6f3ff167be5ab06340d34a6a1200d5a23e0b7fd4
av.dias <av.dias@campus.fct.unl.pt>
2023-10-13 12:53:28 +0100

    [Color] Lighter background color

--- utility/colors.js
+++ utility/colors.js
@@ -1,11 +1,14 @@
 export const color = {
   backgroundDark: "rgba(26, 34, 56,1)",
-  backgroundLight: "rgba(227, 242, 253,1)",
-  complementary: "rgba(255, 255, 255, 0.5)",
+  backgroundLight: "rgba(242, 249, 253,1)",
+  //backgroundLight: "rgba(227, 242, 253,1)",
+  complementary: "rgba(255, 255, 255, 1)",
+  //complementary: "rgba(253, 253, 253, 1)",
   glass: "rgba(184, 191, 214, 0.1)",
   glassBorder: "rgba(211,211,211,1)",
   calendar: "white",
   calendarBorder: "white",
   secundary: "rgba(255, 217, 90, 1)",
-  button: "rgba(229, 124, 35, 1)",
+  button: "rgba(236, 158, 91, 1)",
+  //button: "rgba(229, 124, 35, 1)",
 };
