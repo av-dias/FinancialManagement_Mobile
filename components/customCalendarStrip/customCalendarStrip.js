c7b459e4f090fa94ea2bbe1db980990adbf622c8
av.dias <av.dias@campus.fct.unl.pt>
2023-10-28 19:24:40 +0100

    [Component] CalendarStrip

---
+++ components/customCalendarStrip/customCalendarStrip.js
@@ -0,0 +1,110 @@
+import { Text, View, TextInput, Pressable } from "react-native";
+import { _styles } from "./style";
+import { horizontalScale, verticalScale, moderateScale, heightTreshold } from "../../utility/responsive";
+import CalendarStrip from "react-native-calendar-strip";
+import React, { useState, useEffect } from "react";
+import CalendarPicker from "react-native-calendar-picker";
+import { color } from "../../utility/colors";
+import CardWrapper from "../../components/cardWrapper/cardWrapper";
+import { MaterialIcons, FontAwesome, MaterialCommunityIcons, AntDesign, FontAwesome5, Ionicons, Entypo } from "@expo/vector-icons";
+
+const BORDER_RADIUS = 10;
+
+export default function CustomCalendarStrip({ pickerCurrentDate, setPickerCurrentDate }) {
+  const styles = _styles;
+  const [datePicker, setDatePicker] = useState(true);
+
+  const calendarPicker = () => {
+    setDatePicker(!datePicker);
+  };
+
+  const changeDateCalendar = (date) => {
+    calendarPicker();
+    setPickerCurrentDate(new Date(date));
+  };
+
+  return (
+    <>
+      {datePicker ? null : (
+        <CardWrapper
+          style={{
+            position: "absolute",
+            alignSelf: "center",
+            zIndex: 1,
+          }}
+        >
+          <CalendarPicker
+            width={horizontalScale(345)}
+            onDateChange={(date) => {
+              changeDateCalendar(date);
+            }}
+            todayBackgroundColor="transparent"
+            todayTextStyle={{
+              color: "gray",
+              fontWeight: "bold",
+              padding: 5,
+              textAlign: "center",
+              justifyContent: "center",
+            }}
+            monthYearHeaderWrapperStyle={{
+              color: "black",
+              borderWidth: 1,
+              borderRadius: BORDER_RADIUS,
+              padding: horizontalScale(3),
+              borderColor: "white",
+              backgroundColor: "white",
+            }}
+            monthTitleStyle={{ fontWeight: "bold", fontSize: verticalScale(15) }}
+            yearTitleStyle={{ fontWeight: "bold", fontSize: verticalScale(15) }}
+            selectedDayColor="red"
+            startFromMonday={true}
+            initialDate={pickerCurrentDate}
+          />
+          <View style={{ marginTop: -verticalScale(26) }}>
+            <Pressable
+              style={{ alignSelf: "flex-end", backgroundColor: "transparent", marginTop: -verticalScale(5) }}
+              onPress={() => {
+                changeDateCalendar(pickerCurrentDate);
+              }}
+            >
+              <AntDesign style={{ padding: verticalScale(5) }} name="closecircle" size={verticalScale(20)} color="black" />
+            </Pressable>
+          </View>
+        </CardWrapper>
+      )}
+      <CalendarStrip
+        ref={(component) => (this._calendar = component)}
+        selectedDate={pickerCurrentDate}
+        calendarAnimation={{ type: "sequence", duration: 15 }}
+        daySelectionAnimation={{ type: "border", duration: 100, borderWidth: 1, borderHighlightColor: color.calendarBorder }}
+        style={{ height: verticalScale(80), backgroundColor: color.complementary, borderRadius: BORDER_RADIUS, elevation: 2 }}
+        calendarHeaderStyle={{
+          color: "black",
+          marginTop: 5,
+          padding: horizontalScale(3),
+          fontSize: verticalScale(15),
+          borderWidth: 1,
+          borderRadius: BORDER_RADIUS,
+          borderColor: "white",
+          backgroundColor: "white",
+          elevation: 2,
+        }}
+        calendarColor={"transparent"}
+        dateNumberStyle={{ color: "black", fontSize: verticalScale(15) }}
+        dateNameStyle={{ color: "black", fontSize: verticalScale(10) }}
+        highlightDateNumberStyle={{ color: color.secundary, fontSize: verticalScale(20) }}
+        highlightDateNameStyle={{ color: color.secundary }}
+        disabledDateNameStyle={{ color: "grey" }}
+        disabledDateNumberStyle={{ color: "grey" }}
+        iconContainer={{ flex: 0.1 }}
+        startFromMonday={true}
+        scrollable={true}
+        scrollerPaging={true}
+        onDateSelected={setPickerCurrentDate}
+        onHeaderSelected={() => {
+          calendarPicker();
+        }}
+      />
+    </>
+  );
+}
---
+++ components/customCalendarStrip/style.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View, TextInput, Image, Pressable } from "react-native";
+import { color } from "../../utility/colors";
+import { horizontalScale, verticalScale, moderateScale, heightTreshold } from "../../utility/responsive";
+import { StatusBar, Dimensions } from "react-native";
+
+const borderRadius = 10;
+
+export const _styles = StyleSheet.create({});
