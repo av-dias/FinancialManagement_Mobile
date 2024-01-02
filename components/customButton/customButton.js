d926933e8f9b7c7eb636344465f43094e5c79256
av.dias <av.dias@campus.fct.unl.pt>
2024-01-02 12:58:04 +0000

    [Component] Custom button with flexible text

--- components/customButton/customButton.js
+++ components/customButton/customButton.js
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, View, TextInput, Image, Pressable, TouchableOpacity, ScrollView, Alert } from "react-native";
 import { _styles } from "./style";
 
-export default function CustomButton({ handlePress }) {
+export default function CustomButton({ handlePress, text = "Submit" }) {
   const styles = _styles;
 
   return (
     <View style={styles.submitButton}>
       <Pressable style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Submit</Text>
+        <Text style={styles.buttonText}>{text}</Text>
       </Pressable>
     </View>
   );
