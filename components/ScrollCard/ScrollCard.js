059c51a5a8fafd71f2e480136e5590ccce989e97
av.dias <av.dias@campus.fct.unl.pt>
2024-01-03 15:13:43 +0000

    [Component] Fixed minor problem on TypeCard

--- components/ScrollCard/ScrollCard.js
+++ components/ScrollCard/ScrollCard.js
@@ -11,7 +11,6 @@ import commonStyles from "../../utility/commonStyles";
 
 export default function TypeCard({ setItem, itemList }) {
   const styles = _styles;
-  const [datePicker, setDatePicker] = useState(false);
 
   return (
     <View style={styles.calendarContainer}>
@@ -34,76 +33,11 @@ export default function TypeCard({ setItem, itemList }) {
                   backgroundColor: "transparent",
                 }}
               >
-                <Pressable onPress={() => setDatePicker(true)}>
-                  <Text style={{ textAlign: "center", fontSize: verticalScale(15) }}>{itemList[index]}</Text>
-                </Pressable>
+                <Text style={{ textAlign: "center", fontSize: verticalScale(15) }}>{itemList[index]}</Text>
               </View>
             )}
           />
         </View>
-        {datePicker && (
-          <ModalCustom modalVisible={datePicker} setModalVisible={setDatePicker} size={6} hasColor={false}>
-            <View
-              style={{
-                ...styles.rowGap,
-                maxWidth: verticalScale(300),
-                maxHeight: verticalScale(100),
-                flex: 1,
-                alignItems: "center",
-                gap: verticalScale(20),
-                backgroundColor: color.complementary,
-                borderRadius: commonStyles.borderRadius,
-              }}
-            >
-              <View style={{ width: verticalScale(120), height: verticalScale(70) }}>
-                <ScrollView vertical={true}>
-                  {months.map((month, index) => {
-                    return (
-                      <Pressable
-                        key={"P" + month}
-                        style={{ backgroundColor: color.complementary, paddingVertical: verticalScale(5), marginVertical: verticalScale(5) }}
-                        onPress={() => {
-                          setItem(index);
-                        }}
-                      >
-                        <Text key={"T" + month} style={{ fontSize: verticalScale(20), textAlign: "center", justifyContent: "center" }}>
-                          {month}
-                        </Text>
-                      </Pressable>
-                    );
-                  })}
-                </ScrollView>
-              </View>
-              <View style={{ width: verticalScale(120), height: verticalScale(70) }}>
-                <ScrollView vertical={true}>
-                  {["2022", "2023", "2024", "2025"].map((year) => {
-                    return (
-                      <Pressable
-                        key={"P" + year}
-                        style={{ backgroundColor: color.complementary, paddingVertical: verticalScale(5), marginVertical: verticalScale(5) }}
-                        onPress={() => {
-                          setCurrentYear(year);
-                          setDatePicker(!datePicker);
-                        }}
-                      >
-                        <Text key={"T" + year} style={{ fontSize: verticalScale(20), textAlign: "center", justifyContent: "center" }}>
-                          {year}
-                        </Text>
-                      </Pressable>
-                    );
-                  })}
-                </ScrollView>
-              </View>
-            </View>
-            <Pressable
-              style={{ flex: 1, backgroundColor: "transparent" }}
-              key={"CloseButton"}
-              onPress={() => {
-                setDatePicker(!datePicker);
-              }}
-            ></Pressable>
-          </ModalCustom>
-        )}
       </View>
     </View>
   );
