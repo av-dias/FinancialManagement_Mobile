b71178c86c90309755eccf26b9ed231e999dc89d
av.dias <av.dias@campus.fct.unl.pt>
2024-01-14 19:30:14 +0000

    [Stats] Adding first impression for data stats

--- pages/stats/stats.js
+++ pages/stats/stats.js
@@ -1,12 +1,13 @@
 import { useFocusEffect } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import React, { useState } from "react";
-import { VictoryChart, VictoryLine, VictoryBar, VictoryAxis } from "victory-native";
+import { VictoryChart, VictoryLine, VictoryBar, VictoryScatter } from "victory-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 //Custom Components
 import Header from "../../components/header/header";
 import CardWrapper from "../../components/cardWrapper/cardWrapper";
+import TypeCard from "../../components/typeCard/typeCard";
 
 //Custom Constants
 import { _styles } from "./style";
@@ -22,8 +23,9 @@ export default function Stats({ navigation }) {
 
   const [email, setEmail] = useState("");
 
-  const [spendByType, setSpendByType] = useState({ [STATS_TYPE[0]]: [[""]] });
-  const [purchaseTotalByDate, setPurchaseTotalByDate] = useState([{ x: "Jan", y: 0 }]);
+  const [purchaseTotalByDate, setPurchaseTotalByDate] = useState({ currentYear: [{ x: "Jan", y: 0 }], lastYear: [{ x: "Jan", y: 0 }] });
+  const [spendByType, setSpendByType] = useState([{ x: "Supermarket", y: 0 }]);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
   useFocusEffect(
     React.useCallback(() => {
@@ -32,27 +34,42 @@ export default function Stats({ navigation }) {
         let email = await getUser();
         setEmail(email);
 
-        let auxPurchaseTotal = {},
+        let auxCurrentPurchaseTotal = {},
+          auxPreviousPurchaseTotal = {},
           auxSpendByType = {};
-
-        let currentYear = 2024;
 
         for (let type of Object.values(STATS_TYPE)) {
           // Current Month Data
           let resPurchaseStats = await getPurchaseStats(email, currentYear, type).catch((error) => console.log(error));
           auxSpendByType[type] = resPurchaseStats;
 
-          let resPurchaseTotal = await getPurchaseTotal(email, currentYear, type).catch((error) => console.log(error));
-          auxPurchaseTotal[type] = resPurchaseTotal;
+          let resCurrentPurchaseTotal = await getPurchaseTotal(email, currentYear, type).catch((error) => console.log(error));
+          auxCurrentPurchaseTotal[type] = resCurrentPurchaseTotal;
+
+          let resPreviousPurchaseTotal = await getPurchaseTotal(email, currentYear - 1, type).catch((error) => console.log(error));
+          auxPreviousPurchaseTotal[type] = resPreviousPurchaseTotal;
         }
 
-        let auxPurchaseTotalByDate = [];
+        let auxPurchaseTotalByDate = { currentYear: [], lastYear: [] };
         let auxPurchaseSpendByType = [];
+        let value = [];
 
-        for (let date of Object.keys(auxPurchaseTotal[STATS_TYPE[0]])) {
-          let purchaseTotal = parseFloat(auxPurchaseTotal[STATS_TYPE[0]][date]);
-          let purchasePersonal = parseFloat(auxPurchaseTotal[STATS_TYPE[1]][date]);
-          auxPurchaseTotalByDate.push({ x: months[date], y: purchaseTotal - purchasePersonal });
+        for (let date of Object.keys(auxCurrentPurchaseTotal[STATS_TYPE[0]])) {
+          let purchaseTotal = parseFloat(auxCurrentPurchaseTotal[STATS_TYPE[0]][date]);
+          let purchasePersonal = parseFloat(auxCurrentPurchaseTotal[STATS_TYPE[1]][date]);
+          auxPurchaseTotalByDate["currentYear"].push({ x: months[date], y: purchaseTotal - purchasePersonal });
+        }
+
+        if (auxPreviousPurchaseTotal[STATS_TYPE[0]] != {}) {
+          for (let date of Object.keys(auxPreviousPurchaseTotal[STATS_TYPE[0]])) {
+            let purchaseTotal = parseFloat(auxPreviousPurchaseTotal[STATS_TYPE[0]][date]);
+            let purchasePersonal = parseFloat(auxPreviousPurchaseTotal[STATS_TYPE[1]][date]);
+            auxPurchaseTotalByDate["lastYear"].push({ x: months[date], y: purchaseTotal - purchasePersonal });
+          }
+
+          value = auxPurchaseTotalByDate["lastYear"].filter(
+            (value) => !auxPurchaseTotalByDate["currentYear"].map((currentYear) => currentYear.x).includes(value.x)
+          );
         }
 
         for (let type of Object.keys(auxSpendByType[STATS_TYPE[0]])) {
@@ -61,52 +78,85 @@ export default function Stats({ navigation }) {
           auxPurchaseSpendByType.push({ x: type, y: purchaseTotal + purchasePersonal });
         }
 
-        setPurchaseTotalByDate(auxPurchaseTotalByDate);
+        setPurchaseTotalByDate({ currentYear: auxPurchaseTotalByDate["currentYear"], lastYear: value });
         setSpendByType(auxPurchaseSpendByType);
-        console.log(auxPurchaseTotalByDate);
-        console.log(auxPurchaseSpendByType);
       }
       fetchData();
-    }, [])
+    }, [currentYear])
   );
+
+  const getMaxArrayObject = (arr, offset) => {
+    let max = arr.reduce((acc, value) => {
+      if (value.y > acc) {
+        return value.y;
+      }
+      return acc;
+    }, 0);
+
+    return max + offset;
+  };
+
+  const getSumArrayObject = (arr) => {
+    if (!arr || arr.length == 0) return 0;
+    return arr.reduce((acc, value) => acc + parseFloat(value.y), 0);
+  };
 
   return (
     <LinearGradient colors={["#121212", "#121212", "#121212", "#000000"]} style={styles.page}>
       <Header email={email} navigation={navigation} />
       <View style={styles.usableScreen}>
         <View style={{ flex: 1, gap: verticalScale(10) }}>
+          <View style={{ alignSelf: "center", flexDirection: "row", gap: 10 }}>
+            <TypeCard setItem={setCurrentYear} itemList={[2024, 2023]} />
+            <CardWrapper style={{ width: verticalScale(100), alignItems: "center" }}>
+              <Text>Split: {getSumArrayObject(purchaseTotalByDate["currentYear"])}€</Text>
+            </CardWrapper>
+          </View>
           <CardWrapper style={{ flex: 1, justifyContent: "center", alignItems: "center", elevation: 0 }}>
             <View style={styles.chart}>
-              <VictoryLine
-                domain={{ x: [0, 12], y: [-40, 1200] }}
-                padding={20}
-                style={{
-                  data: { stroke: "#c43a31" },
-                  parent: { border: "1px solid #ccc" },
-                }}
-                categories={{ x: months }}
-                data={purchaseTotalByDate}
-                interpolation="natural"
-                labels={({ datum }) => datum.x + "\n" + datum.y + "€"}
-              />
-            </View>
-          </CardWrapper>
-          <CardWrapper style={{ flex: 1, justifyContent: "center", alignItems: "center", elevation: 0 }}>
-            <View style={styles.chart}>
-              <VictoryChart>
-                <VictoryBar
-                  domain={{ y: [-40, 8000] }}
-                  domainPadding={20}
+              <View style={{ position: "absolute" }}>
+                <VictoryLine
+                  domain={{ x: [0, 13], y: [-40, getMaxArrayObject(purchaseTotalByDate["currentYear"], 200)] }}
                   padding={20}
                   style={{
                     data: { stroke: "#c43a31" },
                     parent: { border: "1px solid #ccc" },
                   }}
-                  data={spendByType}
+                  categories={{ x: months }}
+                  data={purchaseTotalByDate["currentYear"]}
                   interpolation="natural"
-                  labels={({ datum }) => datum.y + "€"}
+                  labels={({ datum }) => datum.x + "\n" + datum.y + "€"}
                 />
-              </VictoryChart>
+              </View>
+              {purchaseTotalByDate["lastYear"].length != 0 && (
+                <View style={{ position: "absolute" }}>
+                  <VictoryScatter
+                    style={{ data: { fill: "blue" }, labels: { fontSize: 8 } }}
+                    size={3}
+                    categories={{ x: months }}
+                    data={purchaseTotalByDate["lastYear"]}
+                    labels={({ datum }) => datum.x + "\n" + datum.y + "€"}
+                  />
+                </View>
+              )}
+            </View>
+          </CardWrapper>
+          <CardWrapper style={{ flex: 1, justifyContent: "center", alignItems: "center", elevation: 0 }}>
+            <View style={styles.chart}>
+              <VictoryBar
+                horizontal
+                cornerRadius={{ top: 5 }}
+                domain={{ y: [-40, getMaxArrayObject(spendByType, 2000)] }}
+                domainPadding={20}
+                padding={30}
+                style={{
+                  data: { stroke: "#c43a31" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={spendByType}
+                interpolation="natural"
+                labels={({ datum }) => datum.y + "€\n" + datum.x}
+              />
             </View>
           </CardWrapper>
         </View>
